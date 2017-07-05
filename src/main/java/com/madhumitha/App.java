4699f0ef596105779ffa5368921612e38ad7ace2
madhumithaj <madhuj5296@gmail.com>
2017-07-05 15:43:53 +0530

    src adding
    adding src frm maven project

---
+++ src/main/java/com/madhumitha/App.java
@@ -0,0 +1,17 @@
+package com.madhumitha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	public long add(int number1, int number2) {
+		return number1 - number2;
+	}
+}
---
+++ src/test/java/com/madhumitha/AppTest.java
@@ -0,0 +1,42 @@
+package com.madhumitha;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import com.madhumitha.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+		App app = new App();
+		long result = app.add(10, 5);
+		assertEquals("The result is not equal...", 15, result);
+        assertTrue( true );
+    }
+}
