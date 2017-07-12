0e94d635e325ddb1da430e23d60111fd0df43265
madhu <madhuj5296@gmail.com>
2017-07-12 15:30:56 +0530

    1

--- src/main/java/com/madhumitha/LargestOfThreeNumbers.java
+++ src/main/java/com/madhumitha/LargestOfThreeNumbers.java
@@ -22,7 +22,7 @@ class LargestOfThreeNumbers
          System.out.println("Third number is largest.");
       else   
          System.out.println("Entered numbers are not distinct.");
-System.out.println("success... 123")
+System.out.println("success... 123...1");
 
    }
 }
